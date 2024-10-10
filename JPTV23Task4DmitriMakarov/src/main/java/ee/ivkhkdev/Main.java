a21acc35d3a65bd658a090a837ad2a8f0896a4e7
Dmitri Makarov <dimamakaronkin@gmail.com>
2024-10-10 09:13:27 +0300

    first commit

---
+++ JPTV23Task4DmitriMakarov/src/main/java/ee/ivkhkdev/Main.java
@@ -0,0 +1,18 @@
+package ee.ivkhkdev;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] randomnum = new int[100];
+        int sum = 0;
+        for (int i = 0; i < 100; ++i) {
+            int num = random.nextInt(99);
+            randomnum[i] = num;
+            sum += num;
+            System.out.println(num);
+        }
+        double average = (double) sum / (double) randomnum.length;
+        System.out.println("Average Arithmetic is " + average);
+    }
+}
